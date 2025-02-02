a624a8b374b561ee5b279e3bd201213bb7177cdc
Lau Zi Fu <lau.zifu@gmail.com>
2025-02-02 16:01:10 +0800

    Complete new book add functionality

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const myLibrary = [];
+const library = document.querySelector("#library-content");
+const addBookBtn = document.querySelector("#add-book");
+const confirmBtn = document.querySelector("#confirm-btn");
+const formDialog = document.querySelector("#form-dialog");
+const inputTitle = document.querySelector("#title");
+const inputAuthor = document.querySelector("#author");
+const inputPages = document.querySelector("#pages");
+const bookForm = document.querySelector("#form-dialog > form");
+
 
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    var index = null;
+
+    this.getLibraryIndex = function(){
+        return index;
+    }
+
+    this.setLibraryIndex = function(){
+        index = myLibrary.includes(this) ? myLibrary.indexOf(this) : null;
+    }
 }
 
 Book.prototype.getIsRead = function() {
@@ -12,10 +30,65 @@ Book.prototype.getIsRead = function() {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${isReadStr}`;
 } 
 
-function addBookToLibrary(title, author, pages, isRead) {
+
+Book.prototype.createBookCard = function(){
+    const div = document.createElement("div");
+    div.setAttribute("data-LibraryId", this.getLibraryIndex());
+
+    const title = document.createElement("h2");
+    title.textContent = this.title;
+    const author = document.createElement("div");
+    author.textContent = this.author;
+    [title, author].forEach((e) => div.appendChild(e));
+
+    return div;
+}
+
+
+function addBookToLibrary(title, author, pages, isRead=false) {
     const book = new Book(title, author, pages, isRead);
     myLibrary.push(book);
 }
 
-addBookToLibrary("lol", "lol", 123, true);
-console.log(myLibrary[0].getIsRead());
+
+function displayBooks(bookStyleClass){
+    for(const book of myLibrary){
+        book.setLibraryIndex();
+        if(![...library.children]
+        .some(child => child.getAttribute("data-LibraryId") === book.getLibraryIndex())){
+            let bookCard = book.createBookCard();
+            bookCard.classList.toggle(bookStyleClass);
+            library.appendChild(bookCard);
+        }
+    }
+}
+
+
+addBookBtn.addEventListener("click", () => {
+    formDialog.showModal();
+});
+
+
+formDialog.addEventListener("close", (e)=> {
+    let response = formDialog.returnValue;
+
+    if(response !== "cancel" && response !== "default"){
+        response = JSON.parse(response);
+        console.log(response);
+        addBookToLibrary(response.author, response.title, response.pages);
+        displayBooks("book");
+    } 
+
+    bookForm.reset();
+});
+
+
+bookForm.addEventListener("submit", (e)=>{
+    confirmBtn.value = JSON.stringify({
+        author: inputAuthor.value,
+        title: inputTitle.value,
+        pages: inputPages.value
+    });
+});
+
+
