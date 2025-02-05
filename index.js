142d7fc374694762498303b6c3b6d84dad4cad60
Lau Zi Fu <lau.zifu@gmail.com>
2025-02-05 16:55:18 +0800

    Complete refactor class paradigm

--- index.js
+++ index.js
@@ -1,141 +1,135 @@
-const myLibrary = [];
-const library = document.querySelector("#library-content");
-const addBookBtn = document.querySelector("#add-book");
-const confirmBtn = document.querySelector("#confirm-btn");
-const formDialog = document.querySelector("#form-dialog");
-const inputTitle = document.querySelector("#title");
-const inputAuthor = document.querySelector("#author");
-const inputPages = document.querySelector("#pages");
-const bookForm = document.querySelector("#form-dialog > form");
+"use strict";
 
+class Book {
+    #read = false;
 
-function Book(title, author, pages, isRead) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.isRead = isRead;
-    var index = null;
-
-    this.getLibraryIndex = function(){
-        return index;
+    constructor(title, author, pages = 0, isRead = false){
+        this.title = title;
+        this.author = author;
+        if (Number.isInteger(pages)) this.pages = pages;
+        this.isRead = isRead;
     }
 
-    this.setLibraryIndex = function(i){
-        index = i;
+    get isRead(){
+        return this.#read;
+    }
+
+    set isRead(bool){
+        if (bool instanceof Boolean) this.#read = bool; 
+    }
+
+    toogleRead(){
+        this.#read = !this.#read;
     }
 }
 
-Book.prototype.getIsRead = function() {
-    let isReadStr =  this.isRead ? "already read" : "not read yet";
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${isReadStr}`;
-} 
 
-Book.prototype.toggleRead = function(){
-    this.isRead = !this.isRead;
-}
+const library =  new class {
+    #myLibrary = [];
 
+    addBook(title, author, pages, isRead){
+        this.#myLibrary.push(new Book(title, author, pages, isRead));
+    }
 
-function createBookCard(book, index) {
-    const div = document.createElement("div");
-    div.classList.add("book");
-    div.dataset.libraryId = index; // Store index dynamically
+    removeBook(index){
+        if (this.#myLibrary[index]) {
+            this.#myLibrary.splice(index, 1);
+        } else {
+            throw new Error ("Book does not exist in library");
+        }
+    }
+    
+    getLibrary(){
+        return [...this.#myLibrary];
+    }
 
-    // Title
-    const title = document.createElement("h2");
-    title.textContent = book.title;
-
-    // Author
-    const author = document.createElement("div");
-    author.textContent = `by ${book.author}`;
-
-    // Pages
-    const pages = document.createElement("div");
-    pages.textContent = `Pages: ${book.pages}`;
-
-    // Actions
-    const actions = document.createElement("div");
-    actions.classList.add("book-actions");
-
-    const removeBtn = createButton("Remove", function() {
-        removeBookFromLibrary(parseInt(div.dataset.libraryId));
-    });
-
-    const readBtn = createButton("Toggle Read", function() {
-        toggleBookRead(parseInt(div.dataset.libraryId));
-        this.classList.toggle("is-read", myLibrary[parseInt(div.dataset.libraryId)].isRead);
-    });
-
-    if (book.isRead) readBtn.classList.add("is-read");
-
-    [readBtn, removeBtn].forEach(btn => actions.appendChild(btn));
-    [title, author, pages, actions].forEach(el => div.appendChild(el));
-
-    return div;
-}
-
-//auxillary button creator
-function createButton(textContent, callBack, event="click"){
-    const button = document.createElement("button");
-    button.textContent = textContent;
-    button.addEventListener(event, callBack);
-
-    return button;
-}
-
-
-function addBookToLibrary(title, author, pages, isRead=false) {
-    const book = new Book(title, author, pages, isRead);
-    myLibrary.push(book);
-    displayBooks();
-}
-
-function removeBookFromLibrary(index) {
-    if (index >= 0 && index < myLibrary.length) {
-        myLibrary.splice(index, 1);
-        displayBooks(); // Re-render to fix indices
+    toggleBookRead(index){
+        this.#myLibrary[index].toogleRead();
     }
 }
 
-function toggleBookRead(index) {
-    if (index >= 0 && index < myLibrary.length) {
-        myLibrary[index].toggleRead(); // Ensure the UI reflects the change
+
+const displayController = new class {
+    #library = document.querySelector("#library-content");
+    #addBookBtn = document.querySelector("#add-book");
+    #formDialog = document.querySelector("#form-dialog");
+    #inputTitle = document.querySelector("#title");
+    #inputAuthor = document.querySelector("#author");
+    #inputPages = document.querySelector("#pages");
+    #bookForm = document.querySelector("#form-dialog > form");
+
+    constructor(){
+        this.#addBookBtn.addEventListener("click", () => {
+            this.#formDialog.showModal();
+        });
+        this.#formDialog.addEventListener("close", (e)=> {
+            this.#addBooktoLibrary(e);
+        });
+        this.#updateScreen();
+    }
+
+
+    #updateScreen(){
+        this.#library.innerHTML = "";
+        const myLibrary = library.getLibrary();
+        myLibrary.forEach((book, index) => {
+            const bookCard = this.#createBookCard(index);
+            this.#library.appendChild(bookCard);
+        });
+    }
+
+
+    #createBookCard(index){
+        const div = document.createElement("div");
+        const title = document.createElement("h2");
+        const author = document.createElement("div");
+        const pages = document.createElement("div");
+        const actions = document.createElement("div");
+        const removeBtn = document.createElement("button");
+        const readBtn = document.createElement("button");
+        const myLibrary = library.getLibrary();
+        const book = myLibrary[index];
+        
+        div.classList.add("book");
+        div.dataset.libraryId = index // Store index dynamically
+        title.textContent = book.title;
+        author.textContent = `by ${book.author}`;
+        pages.textContent = `Pages: ${book.pages}`;
+        actions.classList.add("book-actions");
+        removeBtn.textContent = "Delete";
+        readBtn.textContent = "Toggle Read";
+
+        removeBtn.addEventListener("click", () => {
+            library.removeBook(parseInt(div.dataset.libraryId));
+            this.#updateScreen();
+        })
+        
+        readBtn.addEventListener("click", (e)=> {
+            library.toggleBookRead(parseInt(div.dataset.libraryId));
+            e.target.classList.toggle("is-read", book.isRead);
+        })
+
+        if (book.isRead) readBtn.classList.add("is-read");
+        [readBtn, removeBtn].forEach(btn => actions.appendChild(btn));
+        [title, author, pages, actions].forEach(el => div.appendChild(el));
+
+        return div;
+    }
+
+    #addBooktoLibrary(e){
+        let response = e.target.returnValue;
+        
+        if(response === "default"){
+            library.addBook(this.#inputTitle.value, 
+                this.#inputAuthor.value, this.#inputPages.value);
+            this.#updateScreen();
+        } 
+
+        this.#bookForm.reset();
     }
 }
 
-//display function
-function displayBooks(){
-    library.innerHTML = ""; // Clear previous books before re-rendering
-    myLibrary.forEach((book, index) => {
-        book.setLibraryIndex(index);
-        const bookCard = createBookCard(book, index);
-        library.appendChild(bookCard);
-    });
-}
 
 
-addBookBtn.addEventListener("click", () => {
-    formDialog.showModal();
-});
-
-
-formDialog.addEventListener("close", ()=> {
-    let response = formDialog.returnValue;
-
-    if(response !== "cancel" && response !== "default"){
-        response = JSON.parse(response);
-        addBookToLibrary(response.title, response.author, response.pages);
-    } 
-
-    bookForm.reset();
-});
-
-
-bookForm.addEventListener("submit", ()=>{
-    confirmBtn.value = JSON.stringify({
-        author: inputAuthor.value,
-        title: inputTitle.value,
-        pages: inputPages.value
-    });
-});
 
 
